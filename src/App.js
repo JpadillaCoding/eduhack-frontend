dade57fc73d5fa304c5713e2407bb635f3672f24
Jose Padilla <jose.apadilla99@gmail.com>
2024-04-04 11:18:09 -0500

    bootstrap along with scaffolding styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
+    <div className="main">
+      <header className="main-header"></header>
+      <main className="main-body">
+        <div className="container">
+          <div class="row">
+            <div class="col-sm">
+              <h1>input</h1>
+            </div>
+            <div class="col-sm">
+              <h1>output</h1>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
